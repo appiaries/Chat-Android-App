7627018225517d1031bbb76df52af7396afd2b58
Kunihiko Miyanaga <miyanaga@ideamans.com>
2012-07-09 05:21:50 +0900

    First commit.

---
+++ src/com/appiaries/demo/AppiariesChatActivity.java
@@ -0,0 +1,13 @@
+package com.appiaries.demo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AppiariesChatActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
